5d75b91629ab8482063d3901b1b663dd9b6dba7d
Wendell <Angulo@gmail.com>
2025-01-30 20:45:05 +0800

    Change from mysql to postgress

--- config.php
+++ config.php
@@ -1,5 +1,12 @@
 <?php
-$db = mysqli_connect("localhost", "root", "", "todo")
-    or
-    die("Connection failed: " . mysqli_connect_error());
+// PostgreSQL connection parameters
+$host = "dpg-cudmlertq21c738h6ucg-a";
+$port = "5432"; // Default PostgreSQL port
+$dbname = "todo";
+$user = "todo_b7bf_user";
+$password = "O4bNxYb0eXY994IRxmP6n2IIBPLe8f1m";
+
+// Establish the connection
+$db = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password")
+    or die("Connection failed: " . pg_last_error());
 ?>
